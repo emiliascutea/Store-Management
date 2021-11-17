5d55d001eb429ac8af3ff4d0a8ca77f3824fe1a7
Emilia Scutea <56334327+emiliascutea@users.noreply.github.com>
2021-11-17 22:49:56 +0200

    Delete javadoc generations directory

--- javadoc generations/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"bll"},{"l":"bll.validators"},{"l":"connection"},{"l":"dao"},{"l":"model"},{"l":"presentation"},{"l":"start"}];updateSearchResults();
